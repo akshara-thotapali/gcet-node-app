615888b1c229ddce2c56ef1fef3289d4ddceba68
akshara-thotapali <thotapalliakshara2@gmail.com>
2025-06-02 20:32:17 +0530

    update

--- index.js
+++ index.js
@@ -1,58 +1,31 @@
 import express from "express";
-import cors from 'cors'
+import cors from "cors";
 const app = express();
 app.listen(8080, () => {
   console.log("Server Started");
 });
-app.use(cors())
+app.use(cors());
 app.get("/", (req, res) => {
   return res.send("Good Morning");
 });
 
-app.get("/greet",(req,res)=>{res.send("Greetings")})
-
-app.get("/name",(req,res)=>{res.send("AKSHARA THOTAPALLI")})
-
-app.get("/weather",(req,res)=>{res.send("31degree")})
-
-app.get("/products",(req,res)=>{
-    const products =[
-        {name:"poduct 1",price:334},
-        {name:"product 2",price:224},
-        {name:"product 3",price:556},
-    ];
-    res.json(products);
+app.get("/greet", (req, res) => {
+  res.send("Greetings");
 });
 
+app.get("/name", (req, res) => {
+  res.send("AKSHARA THOTAPALLI");
+});
 
-// import express from "express";
-// import cors from "cors";
-// const app = express();
-// app.listen(8080, () => {
-//   console.log("Server Started on port 8080");
-// });
+app.get("/weather", (req, res) => {
+  res.send("31degree");
+});
 
-// app.use(express.json());
-// app.use(cors());
-// const users = [];
-
-// app.get("/", (req, res) => {
-//   return res.json(users);
-// });
-
-// app.post("/register", (req, res) => {
-//   const { name, email, pass } = req.body;
-//   users.push({ name, email, pass });
-//   return res.json(users);
-// });
-
-// app.post("/login", (req, res) => {
-//   const { email, pass } = req.body;
-//   const found = users.find(
-//     (value) => value.email === email && value.pass === pass
-//   );
-//   if (!found) {
-//     res.json({message:"User not found"})
-//   }
-//   res.json({...found,token:"123"});
-// });
+app.get("/products", (req, res) => {
+  const products = [
+    { name: "Product 1", price: 34 },
+    { name: "Product 2", price: 64 },
+    { name: "Product 3", price: 45 },
+  ];
+  res.json(products);
+});
